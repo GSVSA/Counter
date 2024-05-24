5f919e2fa20c4ca6329add29c0dc9734b572504b
Anna <shvets.anna.andreevna@gmail.com>
2024-05-24 20:30:35 +0300

    Выполнена базовое задание

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    private var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func incrementCount(_ sender: Any) {
+//        counterLabel.text = "\(counter + 1)"
+        counter += 1
+        counterLabel.text = String(counter)
+    }
+    
 }
 
